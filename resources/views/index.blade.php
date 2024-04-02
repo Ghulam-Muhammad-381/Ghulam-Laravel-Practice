8566d3717ca65c7079bc9ca804e276321030bb1b
Ghulam Muhammad <Ghulam_Muhammad@Ghulams-MBP.localdomain>
2024-04-03 01:53:28 +0500

    Explored various Laravel development aspects including routing, blade templates, layout crafting, database connections, and models/migrations, culminating in the creation of a Tasks app

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('title', 'The List of Tasks!')
+
+@section('content')
+<!-- @if(count($tasks)) -->
+
+<div>
+    @forelse( $tasks as $task )
+        <div>
+            <a
+                href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+        </div>
+    @empty
+        <div>There are no tasks!</div>
+    @endforelse
+</div>
+
+<!-- @endif -->
+@endsection
